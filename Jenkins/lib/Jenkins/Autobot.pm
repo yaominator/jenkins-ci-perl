f1b40320f1f5bef43643627bacb348bf76201f5d
Jiqing Yao <jiqingyao@gmail.com>
2012-05-26 22:10:03 -0400

    add autobot

---
+++ Jenkins/lib/Jenkins/Autobot.pm
@@ -0,0 +1,19 @@
+package Jenkins::Autobot;
+use Carp;
+
+use version; our $VERSION = qv('0.0.1');
+
+
+
+1; # Magic true value required at end of module
+__END__
+
+=head1 NAME
+
+Jenkins Autobot is a system to create Jenkins jobs automatically based on the templates your provided
+
+=head1 VERSION
+
+This document describes Jenkins version 0.0.1
+
+
